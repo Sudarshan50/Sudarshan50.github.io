31c41fd3e49757f51ccbcdd88e31fd0d3e16b3d0
Sudarshan50 <105160284+Sudarshan50@users.noreply.github.com>
2023-08-06 11:59:22 +0530

    Dynamic Clock and Front page bug fixed..

--- Sophomore/iitd.js
+++ Sophomore/iitd.js
@@ -1,3 +1,21 @@
+setInterval(showTimeout,1000);
+function showTimeout()
+{
+    const dateTime = new Date();
+    const currentDay = dateTime.getDay();
+    var hours = dateTime.getHours();
+    var minutes = dateTime.getMinutes();
+    var seconds = dateTime.getSeconds();
+    var date = dateTime.getDate();
+    var month = dateTime.getMonth();
+    var year = dateTime.getFullYear();
+    const array = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    const currentTime = hours+":"+minutes;
+
+    // Showing date and time at the bottom of nav bar
+    document.getElementById("uio").innerHTML = 'Date:- '+ date+"/"+month+"/"+year + " , "+ array[currentDay];
+    document.getElementById("oiu").innerHTML = 'Time:- '+hours+':'+minutes+":"+seconds+" seconds";
+}
 const dateTime = new Date();
 const currentDay = dateTime.getDay();
 var hours = dateTime.getHours();
@@ -8,25 +26,20 @@ var month = dateTime.getMonth();
 var year = dateTime.getFullYear();
 const array = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 const currentTime = hours+":"+minutes;
-
-// Showing date and time at the bottom of nav bar
-document.getElementById("uio").innerHTML = 'Date:- '+ date+"/"+month+"/"+year + " , "+ array[currentDay];
-document.getElementById("oiu").innerHTML = 'Time:- '+hours+':'+minutes+":"+seconds+" seconds";
-
 const shedule = 
 [{
     day: "Monday",
     starTime: ["5:0", "9:31", "11:1", "13:0"],
     endTime: ["9:3",'11:0',"12:0","14:0"],
     name: ["HUL-258","CLL-110",'CML-103','CLL-111(Tut)'],
-    room:['LH-318','LH-108','LH-316','LH-615'],
+    room:['LH-318','LH-108','LH-316','BLOCK-II LT-2'],
 },
 {
     day: "Tuesday",
-    starTime: ["5:0", "10:1", "11:1","13:0"],
-    endTime: ["9:1",'11:0',"12:0",'17:0'],
+    starTime: ["5:0", "10:1", "11:1","14:0"],
+    endTime: ["9:1",'11:0',"12:0",'16:0'],
     name: ["CVL-100","CLL-113",'CLL-111','CLL-113(Prc)'],
-    room:['LH-114','LH-308','LH-408','Chem-Lab'],
+    room:['LH-114','LH-308','LH-408','CSC'],
 },
 {
     day: "Wednesday",
@@ -37,8 +50,8 @@ const shedule =
 },
 {
     day: "Thursday",
-    starTime: ["5:0", "9:31", "11:1",'12:1', "13:1"],
-    endTime: ["9:3",'11:0',"12:0",'13:0',"14:0",],
+    starTime: ["5:0", "9:31", "15:1",'12:1', "13:1"],
+    endTime: ["9:3",'11:0',"16:0",'13:0',"14:0",],
     name: ["HUL-258","CLL-110",'CLL-111','CML-103','HUL-258(Tut)'],
     room:['LH-318','LH-108','LH-108','LH-316','LH-603'],
 },
@@ -55,122 +68,137 @@ const shedule =
 
 if(shedule[0].day == array[currentDay])
 {
-    for(let i = 0;i<4;i++)
-    {
-        if(currentTime>= shedule[0].starTime[i] && currentTime<=shedule[0].endTime[i])
+    if(currentTime <= "14:1")
+    {    for(let i = 0;i<4;i++)
         {
-            document.getElementById("current").innerHTML = shedule[0].name[i]+" At- "+shedule[0].room[i]+ "\n" + " At- "+shedule[0].starTime[i];
-            if(i<3 && currentTime <= shedule[0].starTime[i+1])
+            if(currentTime>= shedule[0].starTime[i] && currentTime<=shedule[0].endTime[i])
             {
-                document.getElementById("upcoming").innerHTML = shedule[0].name[i+1]+" At- "+shedule[0].room[i+1] +"\n" + " At- "+shedule[0].starTime[i+1];
-            }
-            else
-            {
-                document.getElementById("upcoming").innerHTML = "Nothing to do..";
+                document.getElementById("current").innerHTML = shedule[0].name[i]+" At- "+shedule[0].room[i]+ "\n" + " At- "+shedule[0].starTime[i];
+                if(i<3 && currentTime <= shedule[0].starTime[i+1])
+                {
+                    document.getElementById("upcoming").innerHTML = shedule[0].name[i+1]+" At- "+shedule[0].room[i+1] +"\n" + " At- "+shedule[0].starTime[i+1];
+                }
+                else
+                {
+                    document.getElementById("upcoming").innerHTML = "Nothing to do..";
 
+                }
             }
-        }
-        else
-        {
-            document.getElementById("current").innerHTML = "Nothing to do...";
-            document.getElementById("upcoming").innerHTML = " --RELAX--";
         }
     }
+    else
+    {
+        document.getElementById("current").innerHTML = "Nothing to do...";
+        document.getElementById("upcoming").innerHTML = " --RELAX--";
+    }
+
 }
 else if(shedule[1].day == array[currentDay])
 {
-    for(let i = 0;i<4;i++)
+    if(currentTime <= "16:1")
     {
-        if(currentTime>= shedule[1].starTime[i] && currentTime<=shedule[1].endTime[i])
+        for(let i = 0;i<4;i++)
         {
-            document.getElementById("current").innerHTML = shedule[1].name[i]+" At- "+shedule[1].room[i]+"\n" + " At- "+shedule[0].starTime[i];
-            if(i<3)
+            if(currentTime>= shedule[1].starTime[i] && currentTime<=shedule[1].endTime[i])
             {
-                document.getElementById("upcoming").innerHTML = shedule[1].name[i+1]+" At- "+shedule[1].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
-            }
-            else
-            {
-                document.getElementById("upcoming").innerHTML = "Nothing to do..";
+                document.getElementById("current").innerHTML = shedule[1].name[i]+" At- "+shedule[1].room[i]+"\n" + " At- "+shedule[0].starTime[i];
+                if(i<3)
+                {
+                    document.getElementById("upcoming").innerHTML = shedule[1].name[i+1]+" At- "+shedule[1].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
+                }
+                else
+                {
+                    document.getElementById("upcoming").innerHTML = "Nothing to do..";
 
+                }
             }
-        }
-        else
-        {
-            document.getElementById("current").innerHTML = "Nothing to do...";
-            document.getElementById("upcoming").innerHTML = " --RELAX--";
         }
     }
+    else
+    {
+        document.getElementById("current").innerHTML = "Nothing to do...";
+        document.getElementById("upcoming").innerHTML = " --RELAX--";
+    }
+    
 }
 else if(shedule[2].day == array[currentDay])
 {
-    for(let i = 0;i<3;i++)
+    if(currentTime <= "12:1")
     {
-        if(currentTime>= shedule[2].starTime[i] && currentTime<=shedule[2].endTime[i])
+        for(let i = 0;i<3;i++)
         {
-            document.getElementById("current").innerHTML = shedule[2].name[i]+" At- "+shedule[2].room[i]+"\n" + " At- "+shedule[0].starTime[i];
-            if(i<2)
+            if(currentTime>= shedule[2].starTime[i] && currentTime<=shedule[2].endTime[i])
             {
-                document.getElementById("upcoming").innerHTML = shedule[2].name[i+1]+" At- "+shedule[2].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
-            }
-            else
-            {
-                document.getElementById("upcoming").innerHTML = "Nothing to do..";
+                document.getElementById("current").innerHTML = shedule[2].name[i]+" At- "+shedule[2].room[i]+"\n" + " At- "+shedule[0].starTime[i];
+                if(i<2)
+                {
+                    document.getElementById("upcoming").innerHTML = shedule[2].name[i+1]+" At- "+shedule[2].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
+                }
+                else
+                {
+                    document.getElementById("upcoming").innerHTML = "Nothing to do..";
 
+                }
             }
         }
-        else
-        {
-            document.getElementById("current").innerHTML = "Nothing to do...";
-            document.getElementById("upcoming").innerHTML = " --RELAX--";
-        }
+    }
+    else
+    {
+        document.getElementById("current").innerHTML = "Nothing to do...";
+        document.getElementById("upcoming").innerHTML = " --RELAX--";
     }
 }
 else if(shedule[3].day == array[currentDay])
 {
-    for(let i = 0;i<5;i++)
-    {
-        if(currentTime>= shedule[3].starTime[i] && currentTime<=shedule[3].endTime[i])
+    if(currentTime <= "14:1")
+    {    for(let i = 0;i<5;i++)
         {
-            document.getElementById("current").innerHTML = shedule[3].name[i]+" At- "+shedule[3].room[i]+"\n" + " At- "+shedule[0].starTime[i];
-            if(i<4)
+            if(currentTime>= shedule[3].starTime[i] && currentTime<=shedule[3].endTime[i])
             {
-                document.getElementById("upcoming").innerHTML = shedule[3].name[i+1]+" At- "+shedule[3].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
-            }
-            else
-            {
-                document.getElementById("upcoming").innerHTML = "Nothing to do..";
+                document.getElementById("current").innerHTML = shedule[3].name[i]+" At- "+shedule[3].room[i]+"\n" + " At- "+shedule[0].starTime[i];
+                if(i<4)
+                {
+                    document.getElementById("upcoming").innerHTML = shedule[3].name[i+1]+" At- "+shedule[3].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
+                }
+                else
+                {
+                    document.getElementById("upcoming").innerHTML = "Nothing to do..";
 
+                }
             }
         }
-        else
-        {
-            document.getElementById("current").innerHTML = "Nothing to do...";
-            document.getElementById("upcoming").innerHTML = " --RELAX--";
-        }
+    }
+    else
+    {
+        document.getElementById("current").innerHTML = "Nothing to do...";
+        document.getElementById("upcoming").innerHTML = " --RELAX--";
     }
 }
 else if(shedule[4].day == array[currentDay])
 {
-    for(let i = 0;i<4;i++)
+    if(currentTime <= "14:1")
     {
-        if(currentTime>= shedule[4].starTime[i] && currentTime<=shedule[4].endTime[i])
+        for(let i = 0;i<4;i++)
         {
-            document.getElementById("current").innerHTML = shedule[4].name[i]+" At- "+shedule[4].room[i]+"\n" + " At- "+shedule[0].starTime[i];
-            if(i<3)
+            if(currentTime>= shedule[4].starTime[i] && currentTime<=shedule[4].endTime[i])
             {
-                document.getElementById("upcoming").innerHTML = shedule[4].name[i+1]+" At- "+shedule[4].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
-            }
-            else
-            {
-                document.getElementById("upcoming").innerHTML = "Nothing to do..";
+                document.getElementById("current").innerHTML = shedule[4].name[i]+" At- "+shedule[4].room[i]+"\n" + " At- "+shedule[0].starTime[i];
+                if(i<3)
+                {
+                    document.getElementById("upcoming").innerHTML = shedule[4].name[i+1]+" At- "+shedule[4].room[i+1]+"\n" + " At- "+shedule[0].starTime[i+1];;
+                }
+                else
+                {
+                    document.getElementById("upcoming").innerHTML = "Nothing to do..";
 
+                }
             }
         }
-        else
-        {
-            document.getElementById("current").innerHTML = "Nothing to do...";
-            document.getElementById("upcoming").innerHTML = " --RELAX--";
-        }
+    }
+    else
+    {
+        document.getElementById("current").innerHTML = "Nothing to do...";
+        document.getElementById("upcoming").innerHTML = " --RELAX--";
     }
 }
 else
